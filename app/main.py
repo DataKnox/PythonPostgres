b1ef55d39f45b4559f106b7b7a181b759200eaa0
Knox Hutchinson <knox@knoxsdata.com>
2023-01-15 21:29:50 -0600

    add auth

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 import pandas as pd
 import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from fastapi.security.api_key import APIKey
+import auth
 load_dotenv()
 app = FastAPI()
 db_name = 'postgres'
@@ -28,6 +30,6 @@ def read_item(item_id: int, q: str = None):
 
 
 @app.get("/test")
-def testquery():
+def testquery(api_key: APIKey = Depends(auth.get_api_key)):
     df = pd.read_sql("SELECT table_name FROM information_schema.tables", con)
     return df.to_json(orient="records")
