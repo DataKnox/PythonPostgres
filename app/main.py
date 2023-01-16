77a8845cf8d025b73729f97b15b9ccd00e32d0af
Knox Hutchinson <knox@knoxsdata.com>
2023-01-15 20:53:27 -0600

    update conn details

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
-
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
 app = FastAPI()
+db_name = 'postgres'
+db_user = 'postgres'
+db_pass = os.getenv('DBPASS')
+db_host = 'db'
+db_port = '5432'
+
+# Connecto to the database
+db_string = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+db = create_engine(db_string)
+con = db.connect()
 
 
 @app.get("/")
@@ -11,3 +25,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/test")
+def testquery():
+    df = pd.read_sql("SELECT table_name FROM information_schema.tables", con)
+    return df.to_json(orient="records")
